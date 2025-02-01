b0b36e46ce05230d4465fbf591b24297366018da
dev123 <hyunho3445@gmail.com>
2025-02-01 22:34:58 +0900

    2025/02/01 - openai key add

--- h/src/main/java/lh/h/controller/OpenAIController.java
+++ h/src/main/java/lh/h/controller/OpenAIController.java
@@ -1,16 +1,22 @@
 package lh.h.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.Map;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
 
 @Controller
 @RequestMapping("/ai")
 public class OpenAIController {
 
+    /* -- application.properties -- */
+    @Value("${openai.api.key}")
+    private String openaiApiKey;
+
     @GetMapping("/openAIPage")
-    public String openaitest() {
+    public String openAiPage(Model model) {
+        model.addAttribute("apiKey", openaiApiKey);
         return "ai/openAIPage";
     }
 }
